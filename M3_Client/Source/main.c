f0b30707337108fe2608e28b2dc0cb43b16b985a
shin0343 <shin0343@mail.hongik.ac.kr>
2019-10-17 13:06:54 +0900

    Update main.c

--- M3_Client/Source/main.c
+++ M3_Client/Source/main.c
@@ -20,132 +20,132 @@ extern volatile unsigned char Uart2_Rx_Buf[200];
 
 
 int menu[5];
-char arr[5] = {0,'A','C',0,'B'};
-enum key{C1, C1_, D1, D1_, E1, F1, F1_, G1, G1_, A1, A1_, B1, C2, C2_, D2, D2_, E2, F2, F2_, G2, G2_, A2, A2_, B2};
-enum note{N16=BASE/4, N8=BASE/2, N4=BASE, N2=BASE*2, N1=BASE*4};
+char arr[5] = { 0,'A','C',0,'B' };
+enum key { C1, C1_, D1, D1_, E1, F1, F1_, G1, G1_, A1, A1_, B1, C2, C2_, D2, D2_, E2, F2, F2_, G2, G2_, A2, A2_, B2 };
+enum note { N16 = BASE / 4, N8 = BASE / 2, N4 = BASE, N2 = BASE * 2, N1 = BASE * 4 };
 
 int main(void)
 {
 	volatile int isArrived = 0;
 	volatile int isFlipped = 0;
 	float AD_value;
-	char socket=0;
-  signed char x, y, z;
-	int isCustomerHere=0;
-	
+	char socket = 0;
+	signed char x, y, z;
+	int isCustomerHere = 0;
+
 	RCC_GetClocksFreq(&RCC_ClockFreq);
-	
+
 	Key_EXTI_Init();
 	TIM_Config();
 	Uart1_Init(115200);
 	Uart2_Init(115200);
-	
+
 	Lcd_Init();
 	WiFi_Init();
-	
+
 	Factory_Default();
 	wifi_mode = Run_Server();
-  I2C_Setup();
+	I2C_Setup();
 	ADC_Configuration();
-  TIM4_Repeat_Interrupt_Enable(1, 700);
-	
-	volatile unsigned char stroff[]="OFF";
-	
-	for(;;)
+	TIM4_Repeat_Interrupt_Enable(1, 700);
+
+	volatile unsigned char stroff[] = "OFF";
+
+	for (;;)
 	{
-		//¼­¹ö->Å¬¶óÀÌ¾ðÆ® recvÃâ·Â
-		if(Uart2_Cmd_In)
-		{			
-			Uart2_Cmd_In = 0;		
-			Uart1_Printf("\r[recv]: %s\n: ",Uart2_Rx_Buf);	
-			
-			//OFF ¹®ÀÚ¿­ ¼ö½Å½Ã º¸µå Á¤º¸ ÃÊ±âÈ­
-			if(mystrcmp(Uart2_Rx_Buf,stroff)==0)
+		//ì„œë²„->í´ë¼ì´ì–¸íŠ¸ recvì¶œë ¥
+		if (Uart2_Cmd_In)
+		{
+			Uart2_Cmd_In = 0;
+			Uart1_Printf("\r[recv]: %s\n: ", Uart2_Rx_Buf);
+
+			//OFF ë¬¸ìžì—´ ìˆ˜ì‹ ì‹œ ë³´ë“œ ì •ë³´ ì´ˆê¸°í™”
+			if (mystrcmp(Uart2_Rx_Buf, stroff) == 0)
 			{
-				menu[1]=0;
-				menu[2]=0;
-				menu[4]=0;
-				recv_flag=1;
+				menu[1] = 0;
+				menu[2] = 0;
+				menu[4] = 0;
+				recv_flag = 1;
 				Uart1_Printf("POS Reset!\n");
 			}
 		}
-		
-		if(TIM4_Expired)
-    {
-      TIM4_Expired = 0;
-			
-			if(recv_flag)
+
+		if (TIM4_Expired)
+		{
+			TIM4_Expired = 0;
+
+			if (recv_flag)
 			{
-				recv_flag=0;
+				recv_flag = 0;
 			}
-			
-			//Á¶µµ¼¾¼­
+
+			//ì¡°ë„ì„¼ì„œ
 			ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-			if(ADC1_flag)
+			if (ADC1_flag)
 			{
-				if(ADC1_value > 2080)
-				{					
-					isArrived=0;
+				if (ADC1_value > 2080)
+				{
+					isArrived = 0;
 					Lcd_Clr_Screen(0);
 				}
 				else
 				{
-					if(!isArrived)
+					if (!isArrived)
 					{
 						Uart1_Printf("Customer Arrived!\n");
-						isArrived=1;
+						isArrived = 1;
 					}
-					
-					Lcd_Printf(0 ,0,GREEN,RED,1,1,"A : %d",menu[1]);
-					Lcd_Printf(0,20,GREEN,RED,1,1,"B : %d",menu[4]);
-					Lcd_Printf(0,40,GREEN,RED,1,1,"C : %d",menu[2]);
-					
+
+					Lcd_Printf(0, 0, GREEN, RED, 1, 1, "A : %d", menu[1]);
+					Lcd_Printf(0, 20, GREEN, RED, 1, 1, "B : %d", menu[4]);
+					Lcd_Printf(0, 40, GREEN, RED, 1, 1, "C : %d", menu[2]);
+
 				}
-				
-				ADC1_flag = 0;   
+
+				ADC1_flag = 0;
 			}
-			
-			//°¡¼Óµµ¼¾¼­
-      x = I2C_getbyte(0x38,0x29); //X-axis to read..
-      y = I2C_getbyte(0x38,0x2b); //Y-Axis to read..
-      z = I2C_getbyte(0x38,0x2d); //Z Axis to read..
-			
-      //Uart1_Printf("X: %3d, Y: %3d, Z: %3d\n",x, y, z);
-			if(z < 0)
+
+			//ê°€ì†ë„ì„¼ì„œ
+			x = I2C_getbyte(0x38, 0x29); //X-axis to read..
+			y = I2C_getbyte(0x38, 0x2b); //Y-Axis to read..
+			z = I2C_getbyte(0x38, 0x2d); //Z Axis to read..
+
+			//Uart1_Printf("X: %3d, Y: %3d, Z: %3d\n",x, y, z);
+			if (z < 0)
 			{
-				if(!isFlipped)
+				if (!isFlipped)
 				{
 					//send to server
 					Uart2_Send_String("POS Flipped!!\n");
 					Uart1_Printf("POS Flipped!!\n");
-					isFlipped=1;
+					isFlipped = 1;
 				}
-				
+
 				//buzzer
-				TIM3_Buzzer_Beep(C1,N8);	
+				TIM3_Buzzer_Beep(C1, N8);
 				TIM3_Buzzer_Init();
 			}
 			else
 			{
 				isFlipped = 0;
 			}
-			
-    }
-		
-		if(Key_Value){
-			menu[Key_Value]++;			
-			
-			Lcd_Printf(0 ,0,GREEN,RED,1,1,"A : %d",menu[1]);
-			Lcd_Printf(0,20,GREEN,RED,1,1,"B : %d",menu[4]);
-			Lcd_Printf(0,40,GREEN,RED,1,1,"C : %d",menu[2]);
-			
-			
-			Uart1_Printf("%c\n",arr[Key_Value]);
+
+		}
+
+		if (Key_Value) {
+			menu[Key_Value]++;
+
+			Lcd_Printf(0, 0, GREEN, RED, 1, 1, "A : %d", menu[1]);
+			Lcd_Printf(0, 20, GREEN, RED, 1, 1, "B : %d", menu[4]);
+			Lcd_Printf(0, 40, GREEN, RED, 1, 1, "C : %d", menu[2]);
+
+
+			Uart1_Printf("%c\n", arr[Key_Value]);
 			Uart2_Send_Byte(arr[Key_Value]);
-			
+
 			Key_Value = 0;
 		}
-		
+
 	}
-	
+
 }
